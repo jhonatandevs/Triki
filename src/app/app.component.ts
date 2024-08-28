8090988f08c85acdf49271bdfbe0057467bf52bf
JhonatanDev <jhonatan3506@gmail.com>
2024-08-28 18:50:04 -0500

    interfaz de pantalla + botones - Estructura sass - Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotoneraComponent } from "./components/botonera/botonera.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotoneraComponent, BotoneraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
