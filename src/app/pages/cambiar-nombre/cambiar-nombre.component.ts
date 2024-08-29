741e237187201c4a65405a4eef2882e877d3d295
JhonatanDev <jhonatan3506@gmail.com>
2024-08-28 19:34:11 -0500

    agregando persstencia a los datos con LocalStorage

--- src/app/pages/cambiar-nombre/cambiar-nombre.component.ts
+++ src/app/pages/cambiar-nombre/cambiar-nombre.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cambiar-nombre',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './cambiar-nombre.component.html',
   styleUrl: './cambiar-nombre.component.scss'
 })
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,13 @@
-import {Injectable, signal } from '@angular/core';
+import {Injectable, signal, effect } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  name= signal<string>("")
-  constructor() { }
+  name= signal<string>("");
+  guardarNombreEnLocalStorage=effect(()=>localStorage.setItem('nombre',this.name()))
+  constructor() { 
+    const nombreLocalStorage=localStorage.getItem('nombre');
+    if(nombreLocalStorage) this.name.set(nombreLocalStorage)
+  }
 }
